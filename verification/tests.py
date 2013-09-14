c8a14c215e95ee999e7c3596298a16948b6e6343
Suwandi Tan <swndtan@gmail.com>
2013-09-14 13:08:56 +0700

    Forgot replace tests

--- verification/tests.py
+++ verification/tests.py
@@ -1,36 +1,38 @@
-"""
-TESTS is a dict with all you tests.
-Keys for this will be categories' names.
-Each test is dict with
-    "input" -- input data for user function
-    "answer" -- your right answer
-    "explanation" -- not necessary key, it's using for additional info in animation.
-"""
-
-
 TESTS = {
-    "Basics": [
-        {
-            "input": [3, 2],
-            "answer": 5,
-            "explanation": "3+2=?"
-        },
-        {
-            "input": [5, 7],
-            "answer": 12,
-            "explanation": "5+7=?"
-        }
-    ],
-    "Extra": [
-        {
-            "input": [6, 3],
-            "answer": 9,
-            "explanation": "6+3=?"
-        },
-        {
-            "input": [6, 7],
-            "answer": 13,
-            "explanation": "6+7=?"
-        }
-    ]
+    "1st": {
+        "input": [(2,5)],
+        "number": [None, 47]
+    },
+    "2nd": {
+        "input": [(3, 7)],
+        "number": [None, 94]
+    },
+    "3rd": {
+        "input": [(0, 2)],
+        "number": [None, 52]
+    },
+    "4th": {
+        "input": [(0, 8)],
+        "number": [None, 64]
+    },
+    "5th": {
+        "input": [(2, 4)],
+        "number": [None, 38]
+    },
+    "6th": {
+        "input": [(2, 4)],
+        "number": [None, 22]
+    },
+    "7th": {
+        "input": [(1, 5)],
+        "number": [None, 76]
+    },
+    "8th": {
+        "input": [(1, 4)],
+        "number": [None, 89]
+    },
+    "9th": {
+        "input": [(1, 2)],
+        "number": [None, 17]
+    }
 }
