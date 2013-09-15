7b73fc3e0aa6f510142e8c8e81625e0cdb94225b
Suwandi Tan <swndtan@gmail.com>
2013-09-15 23:11:29 +0700

    Added 3 more test

--- verification/tests.py
+++ verification/tests.py
@@ -4,35 +4,51 @@ TESTS = {
         "number": 47
     },
     "2nd": {
-        "input": [(3, 7)],
+        "input": [(3,7)],
         "number": 94
     },
     "3rd": {
-        "input": [(0, 2)],
+        "input": [(0,2)],
         "number": 52
     },
     "4th": {
-        "input": [(0, 8)],
+        "input": [(0,8)],
         "number": 64
     },
     "5th": {
-        "input": [(2, 4)],
+        "input": [(2,4)],
         "number": 38
     },
     "6th": {
-        "input": [(2, 4)],
+        "input": [(2,4)],
         "number": 22
     },
     "7th": {
-        "input": [(1, 5)],
+        "input": [(1,5)],
         "number": 76
     },
     "8th": {
-        "input": [(1, 4)],
+        "input": [(1,4)],
         "number": 89
     },
     "9th": {
-        "input": [(1, 2)],
+        "input": [(1,2)],
         "number": 17
-    }
+    },
+    "10th": {
+        "input": [(0,2)],
+        "number": 60
+    },
+    "11st": {
+        "input": [(1,3)],
+        "number": 1
+    },
+    "12nd": {
+        "input": [(0, 4)],
+        "number": 100
+    },
+    "13rd": {
+        "input": [(0, 3)],
+        "number": 99
+    }    
 }
