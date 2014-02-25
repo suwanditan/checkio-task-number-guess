708e82102d8ba946027c72b887bfcbf9931e503f
Valentin Bryukhanov <bvv.mag@gmail.com>
2014-02-25 19:21:20 +1200

    local grader

--- verification/tests.py
+++ verification/tests.py
@@ -1,46 +1,46 @@
 TESTS = {
     " 1st": {
-        "input": [(2,5)],
+        "input": [(2, 5)],
         "number": 47
     },
     " 2nd": {
-        "input": [(3,7)],
+        "input": [(3, 7)],
         "number": 94
     },
     " 3rd": {
-        "input": [(0,2)],
+        "input": [(0, 2)],
         "number": 52
     },
     " 4th": {
-        "input": [(0,8)],
+        "input": [(0, 8)],
         "number": 64
     },
     " 5th": {
-        "input": [(2,4)],
+        "input": [(2, 4)],
         "number": 38
     },
     " 6th": {
-        "input": [(2,4)],
+        "input": [(2, 4)],
         "number": 22
     },
     " 7th": {
-        "input": [(1,5)],
+        "input": [(1, 5)],
         "number": 76
     },
     " 8th": {
-        "input": [(1,4)],
+        "input": [(1, 4)],
         "number": 89
     },
     " 9th": {
-        "input": [(1,2)],
+        "input": [(1, 2)],
         "number": 17
     },
     "10th": {
-        "input": [(0,2)],
+        "input": [(0, 2)],
         "number": 60
     },
     "11st": {
-        "input": [(1,3)],
+        "input": [(1, 3)],
         "number": 1
     },
     "12nd": {
@@ -50,5 +50,5 @@ TESTS = {
     "13rd": {
         "input": [(0, 3)],
         "number": 99
-    }    
+    }
 }
